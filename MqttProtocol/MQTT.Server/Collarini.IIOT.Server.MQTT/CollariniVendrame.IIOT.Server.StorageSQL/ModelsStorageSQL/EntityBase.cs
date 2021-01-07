5de899882034c59b88e7cc727f619a267254ea54
MarcoCollarini <66778071+MarcoCollarini@users.noreply.github.com>
2021-01-07 18:21:01 +0100

    MQTT protocol
    Implementazione mqtt protocol con storizzazione dei dati (detection)

---
+++ MqttProtocol/MQTT.Server/Collarini.IIOT.Server.MQTT/CollariniVendrame.IIOT.Server.StorageSQL/ModelsStorageSQL/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollariniVendrame.IIOT.Server.Models
+{
+    public abstract class EntityBase<T>
+    {
+        public T Entity { get; set; }
+    }
+    public abstract class EntityBase : EntityBase<int>
+    {
+    }
+}
