1bd01f99a6f19d3e34977c635aa9029d827b42ce
MarcoCollarini <66778071+MarcoCollarini@users.noreply.github.com>
2021-01-21 15:42:08 +0100

    Update server AMQP

---
+++ AMQP.Server/Collarini.IIOT.Server.AMQP/CollariniVendrame.IIOT.Server.SpeedConsumer/Worker.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using CollariniVendrame.IIOT.Server.Models;
+using CollariniVendrame.IIOT.Server.ServiceStorageSQL;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace CollariniVendrame.IIOT.Server.Worker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IDetectionRepository _detectionRepoitory;
+        private readonly IConfiguration _configuration;
+        private string _connectionString;
+        private string brokerUrl = "localhost";
+        private string exchange = "scooter";
+        private string queue = "Speed_Sensor";
+        private IConnection connection;
+        private IModel channel;
+        public Worker(ILogger<Worker> logger, IDetectionRepository rep, IConfiguration conf)
+        {
+            _logger = logger;
+            _configuration = conf;
+            _detectionRepoitory = rep;
+            _connectionString = conf.GetConnectionString("SQL_CollariniVendrame");
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var factory = new ConnectionFactory() { HostName = brokerUrl };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: exchange, type: ExchangeType.Direct);
+            channel.QueueDeclare(queue: queue,
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+            channel.QueueBind(queue, exchange, "Speed_Sensor");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var obj = JsonSerializer.Deserialize<DetectionConfiguration>(message);
+                _detectionRepoitory.insertData(obj, _connectionString);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: queue,
+                                    autoAck: true,
+                                    consumer: consumer);
+            
+        }
+    }
+}
---
+++ AMQP.Server/Collarini.IIOT.Server.MQTT/CollariniVendrame.IIOT.Server.StorageSQL/ModelsStorageSQL/ScooterConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollariniVendrame.IIOT.Server.Models
+{
+    public class ScooterConfiguration : EntityBase<ScooterConfiguration>
+    {
+        public int ScooterId { get; set; }
+        public string Brand { get; set; }
+        public string Company { get; set; }
+    }
+}
